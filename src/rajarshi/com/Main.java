bbdef80e67b4fe308472ceeb1f90ecf64577bea0
Rajarshi Mandal <asobondhu@gmail.com>
2018-01-01 10:01:11 +0530

    Trying to make it more simple for the users and more harder for coders.

--- src/rajarshi/com/Main.java
+++ src/rajarshi/com/Main.java
@@ -11,17 +11,14 @@ public class Main {
         // @param phaseNumber set to 0 as we are building it, no items loaded.
         U$1 u$1 = new U$1(10000, 18000, 100, 0);
         // Create a list of U1 rockets and load the items for each phase.
-        List<U$1> u$1sPhase1 = simulation.loadU$1(u$1, 1);
-        List<U$1> u$1sPhase2 = simulation.loadU$1(u$1, 2);
-        // Get the list of rockets and run the simulation
-        simulation.runSimulation(u$1sPhase1, u$1sPhase2);
+        List<U$1> u$1sPhase1 = simulation.loadU$1(u$1);
 
         System.out.println();
 
         U$2 u$2 = new U$2(18000, 29000, 120, 0);
-        List<U$2> u$2sPhase1 = simulation.loadU$2(u$2, 1);
-        List<U$2> u$2sPhase2 = simulation.loadU$2(u$2, 2);
-        simulation.runSimulation(u$2sPhase1, u$2sPhase2);
+        List<U$2> u$2sPhase1 = simulation.loadU$2(u$2);
 
+        simulation.runSimulation(u$1sPhase1, u$2sPhase1);
     }
+
 }
--- src/rajarshi/com/Simulation.java
+++ src/rajarshi/com/Simulation.java
@@ -13,34 +13,32 @@ import java.util.Scanner;
  */
 public class Simulation {
     // Text File Locations
-    private String mPhase1, mPhase2;
+    private List<List<Item>> allItems = new ArrayList<>();
 
     /**
-     * A blank Constructor for this class; used to initialize the instances of this class.
+     * A Constructor for this class; used to initialize the instances of this class.
+     *
+     * @param phases are the text files for phases, separated by commas.
      */
-    Simulation(String phase_1, String phase_2) {
-        mPhase1 = phase_1;
-        mPhase2 = phase_2;
+    Simulation(String... phases) {
+        for (String phase : phases) {
+            // Get the array and add it to anther List{@link #allItems)
+            allItems.add(loadItems(phase));
+        }
     }
 
     /**
      * This method loads all items from a text file and returns an ArrayList of Items.
      *
-     * @param phaseNumber is the phase that contain the Items for each phases.
+     * @param txtFile is the phase that contain the Items for each phases.
      * @return the List of Items stored in an array.
      */
-    private List<Item> loadItems(@NotNull int phaseNumber) {
-        Scanner scanner = null;
+    private List<Item> loadItems(String txtFile) {
+        // List to return.
         List<Item> mItemList = new ArrayList<>();
 
         try {
-            if (phaseNumber == 1) {
-                // Ambiguous Method inside scanner!
-                scanner = new Scanner(new FileReader(mPhase1));
-            }
-            if (phaseNumber == 2) {
-                scanner = new Scanner(new FileReader(mPhase2));
-            }
+            Scanner scanner = new Scanner(new FileReader(txtFile));
             while (scanner.hasNextLine()) {
                 // Split the string into 2 and put them into an array.
                 String[] stringArray = scanner.nextLine().split("=");
@@ -60,125 +58,144 @@ public class Simulation {
     }
 
     /**
-     * This method takes the ArrayList of Items returned from loadItems and starts creating U1 rockets.
+     * This method takes the ArrayList of Items returned from {@link #allItems} and starts creating U1 rockets.
      *
-     * @param u$1         is the {@link U$1} object that this method will work upon. (Pass by Reference!)
-     * @param phaseNumber is the phase number to load the list from; Phase 1 or 2.
+     * @param u$1 is the {@link U$1} object that this method will work upon. (Pass by Reference!)
      * @return the list of U$1 rockets loaded with Items.
      */
     @NotNull
-    List<U$1> loadU$1(U$1 u$1, int phaseNumber) {
+    List<U$1> loadU$1(U$1 u$1) {
         // List to return
         List<U$1> u$1List = new ArrayList<>();
-        // Get the phase to load items
-        List<Item> itemList = loadItems(phaseNumber);
+        // Get the phase count to display
+        int phaseCount = 0;
 
-        while (!itemList.isEmpty()) {
-            // Let's try something new instead of conventional for loop.
-            for (ListIterator<Item> iterator = itemList.listIterator(); iterator.hasNext(); ) {
-                Item item = iterator.next();
-                // If can carry the weight of the Item...
-                if (u$1.canCarry(item)) {
-                    // Store it separately to {@link Rocket#mCurrentCargoWeight)...
-                    u$1.carry(item);
-                    // Remove the item from the Item ArrayList.
-                    iterator.remove();
-                } else {        // Only for performance for large ArrayLists, telling that...
-                    continue;   // Don't go below, stay within the *if.
+        // Do the calculations for each List in {@link #allItems)
+        for (List<Item> allItem : allItems) {
+            // For displaying how many U1 rockets loaded.
+            int rocketsLoaded = u$1List.size();
+            // Copy each list to a new ArrayList to retain main List from being getting destroyed.
+            List<Item> itemList = new ArrayList<>(allItem);
+
+            // Load the items from store to U1s and make place for new items
+            while (!itemList.isEmpty()) {
+                // Let's try something new instead of conventional for loop.
+                for (ListIterator<Item> iterator = itemList.listIterator(); iterator.hasNext(); ) {
+                    Item item = iterator.next();
+                    // If can carry the weight of the Item...
+                    if (u$1.canCarry(item)) {
+                        // Store it separately to {@link Rocket#mCurrentCargoWeight)...
+                        u$1.carry(item);
+                        // Remove the item from the Item ArrayList.
+                        iterator.remove();
+                    } else {        // Only for performance for large ArrayLists, telling that...
+                        continue;   // Don't go below, stay within the *if.
+                    }
+                    // Don't have to read all the contents of the Item ArrayList if the condition matches.
+                    if (u$1.getCurrentCargoWeight() == u$1.getCargoLimit()) {
+                        break;
+                    }
                 }
-                // Don't have to read all the contents of the Item ArrayList if the condition matches.
-                if (u$1.getCurrentCargoWeight() == u$1.getCargoLimit()) {
-                    break;
-                }
+                // Add the items to the U$1 Rocket.
+                u$1List.add(new U$1(u$1.getRocketWeight(), u$1.getMaxWeight(),
+                        u$1.getCost(), u$1.getCurrentCargoWeight()));
+                // Clear the space for next items.
+                u$1.setCurrentCargoWeight(0);
             }
-            // Add the items to the U$1 Rocket.
-            u$1List.add(new U$1(u$1.getRocketWeight(), u$1.getMaxWeight(),
-                    u$1.getCost(), u$1.getCurrentCargoWeight()));
-            // Clear the space for next items.
-            u$1.setCurrentCargoWeight(0);
-        }
 
-        System.out.println("Total U1 Rockets loaded: " + u$1List.size() +
-                ((phaseNumber == 1) ? " for Phase 1" : " for Phase 2"));
+            System.out.println("Total U$1 Rockets loaded for Phase " + (++phaseCount) + ": "
+                    + (u$1List.size() - rocketsLoaded));
+        }
 
         return u$1List;
     }
 
     /**
-     * This method takes the ArrayList of Items returned from loadItems and starts creating U2 rockets.
+     * This method takes the ArrayList of Items returned from {@link #allItems} and starts creating {@link U$2} rockets.
      */
     @NotNull
-    List<U$2> loadU$2(U$2 u$2, int phaseNumber) {
+    List<U$2> loadU$2(U$2 u$2) {
         List<U$2> u$2List = new ArrayList<>();
-        List<Item> itemList = loadItems(phaseNumber);
+        int phaseCount = 0;
 
-        while (!itemList.isEmpty()) {
-            // Using the conventional for loop.
-            for (int i = 0; i < itemList.size(); i++) {
-                if (u$2.canCarry(itemList.get(i))) {
-                    u$2.carry(itemList.get(i));
-                    itemList.remove(i);
-                } else {
-                    continue;
+        for (List<Item> allItem : allItems) {
+            int rocketsLoaded = u$2List.size();
+
+            while (!allItem.isEmpty()) {
+                // Using the conventional for loop.
+                for (int i = 0; i < allItem.size(); i++) {
+                    if (u$2.canCarry(allItem.get(i))) {
+                        u$2.carry(allItem.get(i));
+                        allItem.remove(i);
+                    } else {
+                        continue;
+                    }
+                    if (u$2.getCurrentCargoWeight() == u$2.getCargoLimit()) {
+                        break;
+                    }
                 }
-                if (u$2.getCurrentCargoWeight() == u$2.getCargoLimit()) {
-                    break;
-                }
+
+                u$2List.add(new U$2(u$2.getRocketWeight(), u$2.getMaxWeight(),
+                        u$2.getCost(), u$2.getCurrentCargoWeight()));
+                u$2.setCurrentCargoWeight(0);
             }
 
-            u$2List.add(new U$2(u$2.getRocketWeight(), u$2.getMaxWeight(),
-                    u$2.getCost(), u$2.getCurrentCargoWeight()));
-            u$2.setCurrentCargoWeight(0);
+            System.out.println("Total U$2 Rockets loaded for Phase " + (++phaseCount) + ": "
+                    + (u$2List.size() - rocketsLoaded));
         }
 
-        System.out.println("Total U2 Rockets loaded: " + u$2List.size() +
-                ((phaseNumber == 1) ? " for Phase 1" : " for Phase 2"));
         return u$2List;
     }
 
     /**
      * Method takes an ArrayList of Rockets and calls launch and land methods for each of the rockets in the ArrayList
+     * @param rocketPrototypes are the returned list of U$1 and/or U$2 Rockets.
      */
-    void runSimulation(List<? extends Rocket> phase1Rockets, List<? extends Rocket> phase2Rockets) {
+    @SafeVarargs
+    final void runSimulation(List<? extends Rocket>... rocketPrototypes) {
         // Create Rocket List and add all the U1s and U2s.
         List<Rocket> rockets = new ArrayList<>();
-        rockets.addAll(phase1Rockets);
-        rockets.addAll(phase2Rockets);
+        for (List rocketObjects : rocketPrototypes) {
+            rockets.addAll(rocketObjects);
+            String name = rockets.get(0).getClass().getSimpleName();
+            //rockets.addAll(phase2Rockets);
 
-        int launchRocket = 0; // Total number of rockets launched.
-        int rocketFailed = 0; // Total number of rockets got destroyed.
-        int cost = 0; // Total cost
+            int launchRocket = 0; // Total number of rockets launched.
+            int rocketFailed = 0; // Total number of rockets got destroyed.
+            int cost = 0; // Total cost
 
-        while (!rockets.isEmpty()) {
-            for (ListIterator<Rocket> iterator = rockets.listIterator(); iterator.hasNext(); ) {
-                Rocket rocketAtI = iterator.next();
-                // iterator.next() || iterator.previous() ? same : not same
-                // Basically saving the values for those *unnecessary steps.
-                Rocket rocketAtP = iterator.previous();
-                // It's sceptical to learn using the debugger how the objects (the prototype rockets) withing the
-                // List of Rockets did call their own land and launch methods
-                if (rocketAtI.launch() && rocketAtI.land()) {
-                    launchRocket++;
-                    cost += iterator.next().getCost();
-                    // Launched and landed successfully.
-                    iterator.remove();
+            while (!rockets.isEmpty()) {
+                for (ListIterator<Rocket> iterator = rockets.listIterator(); iterator.hasNext(); ) {
+                    Rocket rocketAtI = iterator.next();
 
-                } else {
-                    launchRocket++;
-                    rocketFailed++;
-                    cost += iterator.next().getCost();
-                    // Unnecessary steps...
-                    // Well, something went Wrong and thus had to create a new Rocket.
-                    iterator.remove();
-                    iterator.add(rocketAtP);
-                    // ...up to this but helpful for learning how ArrayList works.
+                    // iterator.next() || iterator.previous() ? same : not same
+                    // Basically saving the values for those *unnecessary steps.
+                    Rocket rocketAtP = iterator.previous();
+                    // It's sceptical to learn using the debugger how the objects (the prototype rockets) withing the
+                    // List of Rockets did call their own land and launch methods
+                    if (rocketAtI.launch() && rocketAtI.land()) {
+                        launchRocket++;
+                        cost += iterator.next().getCost();
+                        // Launched and landed successfully.
+                        iterator.remove();
+
+                    } else {
+                        launchRocket++;
+                        rocketFailed++;
+                        cost += iterator.next().getCost();
+                        // Unnecessary steps...
+                        // Well, something went Wrong and thus had to create a new Rocket.
+                        iterator.remove();
+                        iterator.add(rocketAtP);
+                        // ...up to this but helpful for learning how ArrayList works.
+                    }
                 }
             }
+            System.out.println();
+            System.out.println("Total " + name + " launched: " + launchRocket);
+            System.out.println("Total lost: " + rocketFailed);
+            System.out.println("Total Cost: $" + cost + " Millions");
         }
-
-        System.out.println("Total Rockets launched: " + launchRocket);
-        System.out.println("Total lost: " + rocketFailed);
-        System.out.println("Total Cost: $" + cost + " Millions");
     }
 
 }
